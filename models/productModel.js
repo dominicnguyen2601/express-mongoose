c0b7309b7f1fc0846b6ad5efa259c3e2f572768a
Dominic Nguyen <120007988+dominicnguyen2601@users.noreply.github.com>
2024-03-13 00:17:41 +0700

    feat: api completed

---
+++ models/productModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please enter a product name"]
+    },
+    quantity: {
+      type: Number,
+      require: true
+    },
+    price: {
+      type: Number,
+      require: true
+    },
+    image: {
+      type: String,
+      require: false
+    }
+  },
+  {
+    timeStamps: true
+  }
+)
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product; 
--- server.js
+++ server.js
@@ -1,12 +1,94 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./models/ProductModel");
 
+app.use(express.json()); //middleware
+app.use(express.urlencoded({extended: false}))
 //routes
 
-app.get('/', (req, res) => {
-  res.send("Hello client , I am from server")
+app.get("/", (req, res) => {
+  res.send("Hello client , I am from server");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("Hello mother fucker yeah");
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products); 
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
 })
 
-app.listen(3000, () => {
-  console.log("Node App is running on 3000")
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product); 
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
 })
+
+app.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product)
+    
+  } catch (error) {
+     console.log(error.message);
+     res.status(500).json({message: error.message})
+  }
+})
+
+app.put('/products/:id', async(req, res) => {
+  try {
+      const {id} = req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body);
+      // we cannot find any product in database
+      if(!product){
+          return res.status(404).json({message: `cannot find any product with ID ${id}`})
+      }
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+      
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) =>{
+  try {
+      const {id} = req.params;
+      const product = await Product.findByIdAndDelete(id);
+      if(!product){
+          return res.status(404).json({message: `cannot find any product with ID ${id}`})
+      }
+      res.status(200).json(product);
+      
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:Dominic2601a@cluster0.jhajmwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database Mongoose");
+    app.listen(3000, () => {
+      console.log("Node App is running on 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
